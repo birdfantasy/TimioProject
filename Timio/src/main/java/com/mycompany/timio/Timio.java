8aaf5bdb5b56c679d8646b2a8e9db3ca644b8b62
birdfantasy <88217222+birdfantasy@users.noreply.github.com>
2026-07-24 20:54:26 +1200

    Commiting first time

---
+++ Timio/src/main/java/com/mycompany/timio/Timio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.timio;
+
+/**
+ *
+ * @author Fatem
+ */
+public class Timio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
