b22a4f053f9544d0a026802265d4fc5e3eb7c50a
birdfantasy <88217222+birdfantasy@users.noreply.github.com>
2026-07-24 21:11:40 +1200

    Made a task class

---
+++ Timio/src/main/java/com/mycompany/timio/Task.java
@@ -0,0 +1,21 @@
+package com.mycompany.timio;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fatem
+ */
+public class Task {
+    public String priority;
+    public int time;
+
+    public Task() {
+        this.priority = " ";
+        this.time = 0;
+    }
+    
+}
