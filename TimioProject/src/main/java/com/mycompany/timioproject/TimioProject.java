827bc727cf84344968bdb39d070035305f82a8f3
birdfantasy <88217222+birdfantasy@users.noreply.github.com>
2026-07-25 14:57:43 +1200

    An initial commit

---
+++ TimioProject/src/main/java/com/mycompany/timioproject/TimioProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.timioproject;
+
+/**
+ *
+ * @author Fatem
+ */
+public class TimioProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
