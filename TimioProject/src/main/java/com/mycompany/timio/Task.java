2e0324aafb1ec04e09ec2abeb0c33ddc6f41a484
birdfantasy <88217222+birdfantasy@users.noreply.github.com>
2026-07-25 15:23:02 +1200

    An initial commit

---
+++ TimioProject/src/main/java/com/mycompany/timio/Task.java
@@ -0,0 +1,30 @@
+package com.mycompany.timio;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fatem
+ */
+public class Task {
+    public String priority;
+    public int time;
+
+    public Task() {
+        this.priority = " ";
+        this.time = 0;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+    
+    
+}
